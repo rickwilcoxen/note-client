7ca3de5cbf592da63349f3a0b14163d313220d2f
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-06-17 21:44:03 -0400

    Created resources page for health resources
    HealthResources will be the json data
    Resources will be the eventual page to view all resources/etc

---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,43 @@
+import React from 'react'
+// import { withRouter } from 'react-router-dom'
+import SolidButton from '../../Shared/SolidButton'
+import { Link } from 'react-router-dom'
+
+const Resources = () => {
+  const backgroundImageUrl = 'https://media.giphy.com/media/UYBDCJjwOd9Re/giphy.gif'
+
+  const homeStyles = {
+    /* Center the heading inside the container */
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    /* Set background image */
+    backgroundImage: `url(${backgroundImageUrl})`,
+    /* Covers the entire width of the div */
+    backgroundSize: 'repeat',
+    /* Center the background */
+    backgroundPosition: 'center',
+
+    /* Take up half the viewport's height, (50 out of 100) */
+    height: '100vh',
+    width: '100vw',
+    color: 'White',
+    textAlign: 'center'
+  }
+  return (
+    <div style={homeStyles}>
+      <div>
+        <h1>One Stop For All Your [Brain]Notes</h1>
+        <p>A safe space for all your notes in one place.</p>
+        <Link to='/About'>
+          <SolidButton>About</SolidButton>
+        </Link>
+        <Link to='/About'>
+          <SolidButton>Mental Health Resources</SolidButton>
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default Resources
